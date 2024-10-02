0cadf401b5c389e5dfe85c0f9709c014e621d6b8
Oystein <oystein.nilsen@se.com>
2024-10-02 04:33:23 -0400

    Lagt til aut-kode

--- final_project/index.js
+++ final_project/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
-const session = require('express-session')
+const session = require('express-session');
+const { authenticated } = require('./router/auth_users.js');
 const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
 
@@ -11,7 +12,29 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+    const username = req.body.username;
+    const password = req.body.password;
+    
+    // Check for missing username or password
+    if (!username || ! password) {
+        return res.status(404).json({ message: "Error logging in"});
+    }
+
+    // Authenticate the user
+    if (authenticatedUser(username, password)) {
+        // Generate JWT access token
+        let accessToken = jwt.sign({
+            data: password
+        }, 'access', { expiresIn: 60 * 60 });
+
+        // Store access token and username in session
+        req.session.authorization = {
+            accessToken, username
+        }
+        return res.status(200).send("User successfully logged into the service");
+    } else {
+        return res.status(208).json({ message: "Invalid Login. Check your username and password"})
+    }
 });
  
 const PORT =5000;
